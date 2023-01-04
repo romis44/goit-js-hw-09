bae807dbb4bce2b5ff4957c6b049a1fbafa35ea6
romis44 <romankovalchuk44@gmail.com>
2023-01-04 17:04:36 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,24 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let timerId = null;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', showNotification);
+
+function showNotification(event) {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  event.currentTarget.disabled = true;
+  stopBtn.removeAttribute('disabled');
+}
+
+stopBtn.addEventListener('click', event => {
+  clearInterval(timerId);
+  event.currentTarget.disabled = true;
+  startBtn.removeAttribute('disabled');
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-function showNotification() {
-  timerId = setInterval(() => {
-    startBtn.disabled = true;
-    console.log(timerId);
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-}
-startBtn.addEventListener('click', showNotification);
-
-stopBtn.addEventListener('click', () => {
-  clearInterval(timerId);
-  startBtn.disabled = false;
-});
-
-// stopBtn.addEventListener('click', evt => clearInterval(timerId));
